842304b4ab1c720e314f776f63739ac6a9cc870c
jashwanth <jashwanth.modakatech@gmail.com>
2024-01-26 16:06:44 +0530

    few bug fixeses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-import MainComponent from "./Components/MainComponent";
-import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+import MainComponent from './Components/MainComponent';
+import './index.css';
 function App() {
   return (
     <BrowserRouter
-      basename={process.env.NODE_ENV == "production" ? "/convertoo" : ""}
+    // basename={process.env.NODE_ENV == "production" ? "/convertoo" : ""}
     >
       <MainComponent />
     </BrowserRouter>
